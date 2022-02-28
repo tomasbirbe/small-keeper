fb41a1a6ac4bbdd47f61f1c6205f69419be7d7c0
Tomas Birbe <tomas.birbe@gmail.com>
2022-02-28 19:13:46 -0300

    Dev: Change transition property of Account card. Use translate instead of top/right/bottom/left

--- src/components/AccountCard/AccountCard.tsx
+++ src/components/AccountCard/AccountCard.tsx
@@ -13,7 +13,7 @@ const AccountCard = ({
       as="form"
       background="secondary"
       borderTopRadius="30px"
-      bottom={isEntryDataOpen ? '0' : '-350px'}
+      bottom="-350px"
       boxShadow="
       0px 0px 0.9px -12px rgba(0, 0, 0, 0.138),
       0px 0px 2.2px -12px rgba(0, 0, 0, 0.198),
@@ -29,9 +29,11 @@ const AccountCard = ({
       paddingInline={8}
       position="absolute"
       spacing={0}
+      transform="auto"
       transitionDuration="400ms"
       transitionProperty="all"
       transitionTimingFunction="ease-in-out"
+      translateY={isEntryDataOpen ? '-350px' : '0'}
       width="100%"
       zIndex={2}
     >
